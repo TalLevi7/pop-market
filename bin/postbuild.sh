3b2964d2bab358639698cecd29217aa2f8d680ef
TalLevi7 <Tal.levi@hotmail.com>
2025-06-29 22:45:01 +0300

    deploy-manifest, postbuild.sh updates

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -4,12 +4,13 @@ set -e
 # Clean previous build bundle
 rm -rf ./.amplify-hosting
 mkdir -p ./.amplify-hosting/compute/default
+mkdir -p ./.amplify-hosting/static
 
 # Copy backend code + dependencies
 cp -r backend/* ./.amplify-hosting/compute/default/
 
-# Copy frontend build output
-cp -r frontend/dist ./.amplify-hosting/static
+# Copy frontend build output (just the contents of /dist, not the folder itself)
+cp -r frontend/dist/* ./.amplify-hosting/static/
 
 # Copy routing manifest
 cp deploy-manifest.json ./.amplify-hosting/deploy-manifest.json
