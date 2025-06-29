79b926912896041ab1ad7029760e9e122801ecdf
TalLevi7 <Tal.levi@hotmail.com>
2025-06-29 03:01:17 +0300

    Make postbuild.sh executable

---
+++ bin/postbuild.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+rm -rf ./.amplify-hosting
+mkdir -p ./.amplify-hosting/compute/default
+# Copy backend
+cp -r backend/* ./.amplify-hosting/compute/default/
+cp -r backend/node_modules ./.amplify-hosting/compute/default/node_modules
+# Copy frontend build
+cp -r frontend/dist ./.amplify-hosting/static
+# Copy manifest
+cp deploy-manifest.json ./.amplify-hosting/deploy-manifest.json
