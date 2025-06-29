e0fafee6fc3c91a14e3c5780ee365484e1c02a18
TalLevi7 <Tal.levi@hotmail.com>
2025-06-29 03:21:03 +0300

    .

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 rm -rf ./.amplify-hosting
 mkdir -p ./.amplify-hosting/compute/default
+cd backend
+npm ci
 # Copy backend
 cp -r backend/* ./.amplify-hosting/compute/default/
 cp -r backend/node_modules ./.amplify-hosting/compute/default/node_modules
